39e69cf1b9932893b3c37e2c36b46ab6d3313c83
azamatamirzhan <student@ALEM-316fd8.alem.school>
2019-10-19 18:56:26 +0600

    1

---
+++ raid1e.go
@@ -0,0 +1,49 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func main() {
+	Raid1e(5, 1)
+	Raid1e(5, 3)
+	Raid1e(1, 1)
+	Raid1e(1, 5)
+}
+
+func Raid1e(x, y int) {
+	if x > 0 && y > 0 {
+		for i := 1; i <= y; i++ {
+			if i == 1 {
+				for j := 1; j <= x; j++ {
+					if j == 1 {
+						z01.PrintRune('A')
+					} else if j == x {
+						z01.PrintRune('C')
+					} else {
+						z01.PrintRune('B')
+					}
+				}
+				z01.PrintRune('\n')
+			} else if i == y {
+				for j := 1; j <= x; j++ {
+					if j == 1 {
+						z01.PrintRune('C')
+					} else if j == x {
+						z01.PrintRune('A')
+					} else {
+						z01.PrintRune('B')
+					}
+				}
+				z01.PrintRune('\n')
+			} else {
+				for j := 1; j <= x; j++ {
+					if j == x || j == 1 {
+						z01.PrintRune('B')
+					} else {
+						z01.PrintRune(' ')
+					}
+				}
+				z01.PrintRune('\n')
+			}
+		}
+	}
+}
