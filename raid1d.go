1f25c718f2803e0b0c06e4e7a97c0b202785f5a8
azamatamirzhan <student@ALEM-316fd8.alem.school>
2019-10-19 18:49:57 +0600

    1

--- raid1d.go
+++ raid1d.go
@@ -1,29 +1,22 @@
-package main
+package student
 
 import "github.com/01-edu/z01"
 
 func main() {
-	Raid1c(5, 3)
-	Raid1c(5, 1)
-	Raid1c(1, 1)
-	Raid1c(1, 5)
+	Raid1d(5, 3)
+	Raid1d(5, 1)
+	Raid1d(1, 1)
+	Raid1d(1, 5)
 }
 
-func Raid1c(x, y int) {
+func Raid1d(x, y int) {
 	if x > 0 && y > 0 {
 		for i := 1; i <= y; i++ {
-			if i == 1 {
+			if i == 1 || i == y  {
 				for j := 1; j <= x; j++ {
-					if j == 1 || j == x {
+					if j == 1 {
 						z01.PrintRune('A')
-					} else {
-						z01.PrintRune('B')
-					}
-				}
-				z01.PrintRune('\n')
-			} else if i == y {
-				for j := 1; j <= x; j++ {
-					if j == 1 || j == x {
+					} else if j == x {
 						z01.PrintRune('C')
 					} else {
 						z01.PrintRune('B')
