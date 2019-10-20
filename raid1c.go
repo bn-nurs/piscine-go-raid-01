2c49cd83b146086d504e3f3ed27b7b11487df7e9
azamatamirzhan <52777027+azamatamirzhan@users.noreply.github.com>
2019-10-20 23:04:50 +0600

    Update and rename raid1c to raid1c.go

--- raid1c.go
+++ raid1c.go
@@ -2,13 +2,6 @@ package student
 
 import "github.com/01-edu/z01"
 
-func main() {
-	Raid1c(5, 3)
-	Raid1c(5, 1)
-	Raid1c(1, 1)
-	Raid1c(1, 5)
-}
-
 func Raid1c(x, y int) {
 	if x > 0 && y > 0 {
 		for i := 1; i <= y; i++ {
